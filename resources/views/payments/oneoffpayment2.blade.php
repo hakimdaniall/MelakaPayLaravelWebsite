4c3cd9221029cee905cb41789d4e77dd647cd5f3
Hakim Danial <79852485+rylaiftw@users.noreply.github.com>
2021-07-13 13:18:41 +0800

    add agreed to terms and condition section

--- resources/views/payments/oneoffpayment2.blade.php
+++ resources/views/payments/oneoffpayment2.blade.php
@@ -85,30 +85,25 @@ MelakaPay
 
             <hr>
 
-            <!-- <div class="row payment">
+            <div class="row payment">
                 <div class="col-md-12">
-                    <h6>Complete the form below.</h6>
+                    <h6>Terms and Condition</h6>
                     <div class="row">
-                        <div class="col-md-3 d-flex justify-content-left align-self-center">
-                            <h6 style="font-weight:bold;">Agency</h6>
+                        <div class="col-md-2 d-flex justify-content-center align-self-center">
+                            <input type="checkbox">
                         </div>
-                        <div class="col-md-9 d-flex justify-content-left align-self-center">
-                            <h6>Pembayaran Bil</h6>
+                        <div class="col-md-10 d-flex justify-content-left align-self-center">
+                            <h6>   I hereby have read and agree with the <a href="#">terms and conditions</a> specified</h6>
                         </div>   
-                       
-                    </div>
-                    <div class="row">
-                        <div class="col-md-3 d-flex justify-content-left align-self-center">
-                            <h6 style="font-weight:bold;">ROC Number</h6>
-                        </div>
-                        <div class="col-md-9">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="" name="ROCNum" value="" />
-                            </div>
-                        </div>
                     </div>
                 </div>
-            </div> -->
+            </div>
+            <hr>
+            <div class="col-lg-4 d-flex justify-content-center align-self-center" data-aos="fade-right">
+                <a href="#" class="back-btn">Back</a>
+                <a href="#" class="pay-btn">Continue</a>
+            </div>
+
           </div>
             
 
